10d2bf645261ce62f9c14ad441a541f889169393
rattleworks <gasimov.abdulali@gmail.com>
2026-01-23 19:17:11 +0100

    chore: remove dead code

--- src/helpers/version.rs
+++ src/helpers/version.rs
@@ -1,5 +1,4 @@
-// pub const VERSION: &str = option_env!("GUITAR_BUILD_OVERWRITE_VERSION").unwrap_or(env!("CARGO_PKG_VERSION")); // require unstable for now
 pub const VERSION: &str = match option_env!("GUITAR_BUILD_OVERWRITE_VERSION") {
-    Some(v) => v,
+    Some(version) => version,
     None => env!("CARGO_PKG_VERSION"),
 };
